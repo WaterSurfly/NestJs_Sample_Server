5632c63f0714badb8b04dd4011d545d304d04c00
WaterSurfly <visuallive4jun@gmail.com>
2022-04-01 18:24:08 +0900

    Modify chat sample

--- src/app/chat/chat.gateway.ts
+++ src/app/chat/chat.gateway.ts
@@ -1,47 +1,51 @@
-import { Logger } from '@nestjs/common';
 import {
-    OnGatewayConnection,
-    OnGatewayDisconnect,
-    OnGatewayInit,
     SubscribeMessage,
     WebSocketGateway,
+    OnGatewayInit,
     WebSocketServer,
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+    WsResponse,
 } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
+import { Socket } from 'socket.io';
+import { Server } from 'ws';
 
-@WebSocketGateway({ namespace: 'chat' })
-export class ChatGateway
-    implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
-{
-    private static readonly logger = new Logger(ChatGateway.name);
+@WebSocketGateway({ namespace: '/chat' })
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+    private readonly logger = new Logger(ChatGateway.name);
 
-    @WebSocketServer()
-    server: Server;
-
-    afterInit() {
-        ChatGateway.logger.debug(`Socket Server Init Complete`);
-    }
-
-    handleConnection(client: Socket) {
-        ChatGateway.logger.debug(
-            `${client.id}(${client.handshake.query['username']}) is connected!`,
-        );
-
-        this.server.emit('msgToClient', {
-            name: `admin`,
-            text: `join chat.`,
-        });
-    }
-
-    handleDisconnect(client: Socket) {
-        ChatGateway.logger.debug(`${client.id} is disconnected...`);
-    }
+    @WebSocketServer() server: Server;
 
     @SubscribeMessage('msgToServer')
-    handleMessage(
-        client: Socket,
-        payload: { name: string; text: string },
-    ): void {
-        this.server.emit('msgToClient', payload);
+    public handleMessage(client: Socket, payload: any): Promise<WsResponse<any>> {
+        this.logger.log(`SendMessage: ${JSON.stringify(payload)}`);
+        return this.server.to(payload.room).emit('msgToClient', payload);
+    }
+
+    @SubscribeMessage('joinRoom')
+    public joinRoom(client: Socket, room: string): void {
+        client.join(room);
+        client.emit('joinedRoom', room);
+        this.logger.log(`JoinedRoom: ${client.id}`);
+    }
+
+    @SubscribeMessage('leaveRoom')
+    public leaveRoom(client: Socket, room: string): void {
+        client.leave(room);
+        client.emit('leftRoom', room);
+        this.logger.log(`leftRoom: ${client.id}`);
+    }
+
+    public afterInit(server: Server): void {
+        return this.logger.log('ChatGateway initialized');
+    }
+
+    public handleDisconnect(client: Socket): void {
+        return this.logger.log(`Client disconnected: ${client.id}`);
+    }
+
+    public handleConnection(client: Socket): void {
+        return this.logger.log(`Client connected: ${client.id}`);
     }
 }
